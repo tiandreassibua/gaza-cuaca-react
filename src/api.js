1334eb5e8d5ca8628e41eac73f0d81aaf658fbfa
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2022-07-30 16:45:40 +0700

    deploy vercel

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3b59259e00mshf5e78084e69aef3p1c5fd1jsnf5c11ff3ff72',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const TIME_API_URL = "https://timeapi.io/api/Time/current/coordinate?";
+// export const TIME_API_KEY = "GQWHCHT4SVZ9";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7ad416c9d42563ff004f810ae465f5e5";
